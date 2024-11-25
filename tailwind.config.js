1c251a6e128d0f5edd87aceab5ff663d17011fc0
SFuadkhanEdu <fuadxanzs-bp216@code.edu.az>
2024-11-25 12:22:02 +0400

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    "./index.html", // Include your main HTML file
+    "./js/**/*.js", // Include all JavaScript files in the 'js' folder
+    "./css/**/*.css" // Include all CSS files in the 'css' folder
+  ],
   theme: {
-    extend: {},
+    extend: {}, // Extend the theme if needed
   },
-  plugins: [],
-}
+  plugins: [], // Add plugins here if required
+};
+
 
